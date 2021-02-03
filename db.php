3dacf40cc5c711cd22be29fa2626550bf1c94923
diell baholli <db47461@ubt-uni.net>
2021-02-03 15:56:44 +0100

    faqet e reja login,register me kod php edhe te lidhuna me databazen dedb1

--- db.php
+++ db.php
@@ -9,4 +9,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Database Connected succesfully"
+
+//kjo e bon lidhjen e databazes edhe e bojm include ne login edhe te register.php 
 ?>
